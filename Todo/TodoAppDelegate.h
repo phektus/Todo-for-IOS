7d360c1fab1cfd4a8262b520866d57352662723b
Arbie Samong <phektus@gmail.com>
2012-06-11 12:10:46 +0800

    Initial Commit

---
+++ Todo/TodoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TodoAppDelegate.h
+//  Todo
+//
+//  Created by Arbie Samong on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Todo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Todo
+//
+//  Created by Arbie Samong on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TodoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TodoAppDelegate class]));
+    }
+}
